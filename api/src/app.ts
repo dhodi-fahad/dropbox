6fd800a16c84e191a53c24edf1f0629ba565533c
dhodi-fahad <dhodifahad@gmail.com>
2024-05-12 00:26:57 +0300

    setup express server

---
+++ api/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from "morgan";
+
+const app = express() // New express instance
+const port = 3500; // Port number
+
+// Express Configuration
+
+app.use(cors()) //Enable CORS
+app.use(helmet()) //Enable Helmet
+app.use(morgan('dev')) // Enable Morgan
+
+
+// Start Express server
+app.listen(port, () => {
+    // Callback function when server is succesfully started
+    console.log(`Server started at http://localhost:${port}`)
+})
+
+// Export Express app
+export default app
