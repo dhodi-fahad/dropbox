16ede00f7d9fdb4c3fb3d60be1f3102909dea1f6
dhodi-fahad <dhodifahad@gmail.com>
2024-05-12 12:03:22 +0300

    add models and controllers layers to routers

--- api/src/routes/users.routes.ts
+++ api/src/routes/users.routes.ts
@@ -1,15 +1,28 @@
 import {Router, Request, Response} from 'express';
+import { createUserControler, deleteUserController, getUserByIdController, getUsersController, updateUserController } from '../controllers/users.controllers';
 
 
 const router = Router()
 
 // Users routes
 router.get('/', (req: Request, res: Response) => {
-    res.send('Users route!')
-})
+    getUsersController(req, res);
+});
 
 router.get('/:id', (req: Request, res: Response) => {
-    res.send(`User ${req.params.id} route!`)
-})
+    getUserByIdController(req, res);
+});
+
+router.post('/', (req: Request, res: Response) => {
+    createUserControler(req, res);
+});
+
+router.put('/:id', (req: Request, res: Response) => {
+    updateUserController(req, res);
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    deleteUserController(req, res);
+});
 
 export default router;
